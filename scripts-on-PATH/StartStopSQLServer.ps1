95f15f35975505ce22d696a932a6e7e6969a0f69
Mitchell Fenner <mfenner@configit.com>
2022-12-22 14:33:59 -0500

    feat: script starts/stops sqlserver

---
+++ scripts-on-PATH/StartStopSQLServer.ps1
@@ -0,0 +1,19 @@
+﻿param(
+    [switch]$Start,
+    [switch]$Stop
+)
+
+$services = @('MSSQLSERVER', 'SQLWriter')
+
+foreach ($service in $services) {
+    if ($Stop) {
+        Write-Host "Stopping $service..." -NoNewLine
+        Stop-Service $service
+        Write-Host " Done."
+    }
+    if ($Start) {
+        Write-Host "Starting $service..." -NoNewLine
+        Start-Service $service
+        Write-Host " Done."
+    }
+}
