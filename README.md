5605e273813397909b59b0ae850d2b1bcb944f22
Mitchell Fenner <mfenner@configit.com>
2022-12-22 14:24:51 -0500

    feat: add a script and reorganize

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-A collection of miscellaneous scripts I've written.
+A collection of miscellaneous scripts I've written.  
+I keep them all in one folder so I can easily add that folder to my PATH.
---
+++ scripts-on-PATH/cleanupLocalBranches.ps1
@@ -0,0 +1,35 @@
+try {
+    Write-Warning "This script will delete branches in this repo that have been deleted on the remote."
+    Write-Warning "It should keep any local branches which were never pushed to the remote, but use at your own risk."
+    
+    $repoPath = Read-Host "What is the path to the repository you'd like to clean?"
+
+    Write-Host "Moving to $repoPath..."
+    if( $(Test-Path $repoPath) -ne $true) {
+        throw "Invalid path."
+    }
+    Set-Location $repoPath
+    
+    $defaultBranch = Read-Host "What is the default branch for this repo?"
+    
+    git checkout $defaultBranch;
+    if ( $LASTEXITCODE -ne 0 ) {
+        throw "Git encountered an error."
+    }
+    
+    Write-Host "Fetching and pruning local references to remote branches..."
+    git fetch --prune;
+    
+    Write-Host "Deleting local branches which do not exist on remote..."
+    git branch -vv | Select-String -Pattern ": gone]" | ForEach-Object { $_.toString().Trim().Split(" ")[0]} | ForEach-Object {git branch -d $_}
+    
+    Write-Host "Done!"
+} 
+catch {
+    Write-Error $_
+    exit
+} 
+finally {
+    Set-Location $PSScriptRoot
+}
+
