c2e08d6539fa6517a989ffb2ce4384879ceda179
Mitchell Fenner <mfenner@configit.com>
2024-01-30 11:31:52 -0500

    update journal script

--- scripts/createJournalEntry.ps1
+++ scripts/createJournalEntry.ps1
@@ -1,12 +1,14 @@
-Function Start-Journal {
-  Set-Location ~/journal
-  $todaysDate = $( Get-Date -Format "yyyy-MM-dd" )
-  $fileName = $todaysDate + ".txt"
-  $journalEntryNumber = (Get-ChildItem ~/journal | Measure-Object ).Count
-  if ( !(Get-ChildItem -Filter "$todaysDate.txt") ) {
-      Write-Output "---- Entry Number $journalEntryNumber" > $fileName
-      Write-Output "---- Date $todaysDate" >> $fileName
-  }
-
-  vim "$fileName"
+$journalDir = Join-Path $HOME journal
+if (!(Test-Path $journalDir)) {
+  New-Item -Type Directory -Path $journalDir | Out-Null
 }
+Set-Location $journalDir
+$todaysDate = $( Get-Date -Format "yyyy-MM-dd" )
+$fileName = $todaysDate + ".txt"
+$journalEntryNumber = (Get-ChildItem $journalDir | Measure-Object ).Count
+if ( !(Get-ChildItem -Filter "$todaysDate.txt") ) {
+    Write-Output "---- Entry Number $journalEntryNumber" > $fileName
+    Write-Output "---- Date $todaysDate" >> $fileName
+}
+
+vim "$fileName"
