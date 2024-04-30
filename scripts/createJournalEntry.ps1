3e183a1be8191c97936f955761d5995c5e48fe92
Mitchell Fenner <mitchfen@pm.me>
2024-04-30 18:51:11 -0400

    update to work with obsidian

--- scripts/createJournalEntry.ps1
+++ scripts/createJournalEntry.ps1
@@ -1,14 +1,17 @@
-$journalDir = Join-Path $HOME journal
+$journalDir = Join-Path $HOME "mindpalace/Journal"
 if (!(Test-Path $journalDir)) {
   New-Item -Type Directory -Path $journalDir | Out-Null
 }
 Set-Location $journalDir
-$todaysDate = $( Get-Date -Format "yyyy-MM-dd" )
-$fileName = $todaysDate + ".txt"
+$todaysDate = Get-Date -Format "yyyy-MM-dd"
+$yesterdaysDate = Get-Date (Get-Date).addDays(-1) -Format "yyyy-MM-dd"
+$tomorrowsDate = Get-Date (Get-Date).addDays(1) -Format "yyyy-MM-dd"
+$fileName = $todaysDate + ".md"
 $journalEntryNumber = (Get-ChildItem $journalDir | Measure-Object ).Count
-if ( !(Get-ChildItem -Filter "$todaysDate.txt") ) {
-    Write-Output "---- Entry Number $journalEntryNumber" > $fileName
-    Write-Output "---- Date $todaysDate" >> $fileName
+if ( !(Get-ChildItem -Filter $fileName) ) {
+    Write-Output "[[ $yesterdaysDate ]]" > $fileName
+    Write-Output "[[ $tomorrowsDate ]]" >> $fileName
+    Write-Output "Entry Number $journalEntryNumber" >> $fileName
 }
 
 vim "$fileName"
