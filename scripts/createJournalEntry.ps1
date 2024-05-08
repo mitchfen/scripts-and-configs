2f11f459bd5c5b177a507503166aff95d0d48334
Mitchell Fenner <mfenner@configit.com>
2024-05-08 13:45:31 -0400

    another journal script update

--- scripts/createJournalEntry.ps1
+++ scripts/createJournalEntry.ps1
@@ -11,17 +11,15 @@ if ($env:OS -like "Windows*" ) {
 else {
 	$journalDir = Join-Path $HOME "dev/obsidian-vault-mitchfen/Journal"
 }
-
 Assert-Directory -DirectoryName $journalDir
 Set-Location $journalDir
+
 $todaysDate = Get-Date
-Assert-Directory -DirectoryName $($todaysDate.Year)
-Set-Location $($todaysDate.Year)
 $todaysDateAsString = Get-Date -Format "yyyy-MM-dd"
 $fileName = $todaysDateAsString + ".md"
 $journalEntryNumber = (Get-ChildItem $journalDir | Measure-Object ).Count
 if ( !(Get-ChildItem -Filter $fileName) ) {
-    Write-Output "[[ $($todaysDate.Year) ]]" >> $fileName
+    Write-Output "[[ Journal/$($todaysDate.Year) ]]" >> $fileName
     Write-Output "Entry Number: $journalEntryNumber" >> $fileName
 }
 
