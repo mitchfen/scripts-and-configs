0d4e02a9ba643f0e6d083c5552968798c517ee8c
Mitchell Fenner <mitchfen@pm.me>
2024-05-08 12:51:06 -0400

    update pacman scrip

--- scripts/updatePacmanPackages.ps1
+++ scripts/updatePacmanPackages.ps1
@@ -1,12 +1,13 @@
 Set-Location ~/aur
-Get-ChildItem -Path . -Directory | ForEach-Object { 
+Get-ChildItem -Path . -Directory | ForEach-Object {
     Set-Location $_.FullName;
     $gitOutput = git pull;
     if ( $gitOutput -like "Already up to date." ) {
         Write-Host -Object "$($_.Name) is already up to date" -ForegroundColor Green
     }
-    else { 
+    else {
         makepkg -si
+				git clean -fxd
     }
 }
 Set-Location $PSScriptRoot
