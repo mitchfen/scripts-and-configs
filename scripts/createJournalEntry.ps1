0189897f23cc7e916a8bb8804c728ea0aafcc503
Mitchell Fenner <mfenner@configit.com>
2024-05-08 08:49:51 -0400

    update journal script

--- scripts/createJournalEntry.ps1
+++ scripts/createJournalEntry.ps1
@@ -1,17 +1,29 @@
-$journalDir = Join-Path $HOME "dev/obsidian-vault-mitchfen/Journal"
-if (!(Test-Path $journalDir)) {
-  New-Item -Type Directory -Path $journalDir | Out-Null
+function Assert-Directory {
+	param( [Parameter(Mandatory)][string]$DirectoryName )
+	if (! (Test-Path $DirectoryName)) {
+	  New-Item -Type Directory -Path $DirectoryName | Out-Null
+	}
 }
+
+if ($env:OS -like "Windows*" ) {
+	$journalDir = "/dev/obsidian-vault-mitchfen/Journal"
+}
+else {
+	$journalDir = Join-Path $HOME "dev/obsidian-vault-mitchfen/Journal"
+}
+
+Assert-Directory -DirectoryName $journalDir
 Set-Location $journalDir
-$todaysDate = Get-Date -Format "yyyy-MM-dd"
-$yesterdaysDate = Get-Date (Get-Date).addDays(-1) -Format "yyyy-MM-dd"
-$tomorrowsDate = Get-Date (Get-Date).addDays(1) -Format "yyyy-MM-dd"
-$fileName = $todaysDate + ".md"
+$todaysDate = Get-Date
+Assert-Directory -DirectoryName $($todaysDate.Year)
+Set-Location $($todaysDate.Year)
+$todaysDateAsString = Get-Date -Format "yyyy-MM-dd"
+$fileName = $todaysDateAsString + ".md"
 $journalEntryNumber = (Get-ChildItem $journalDir | Measure-Object ).Count
 if ( !(Get-ChildItem -Filter $fileName) ) {
-    Write-Output "Yesterday: [[ Journal/$yesterdaysDate ]]" > $fileName
-    Write-Output "Tomorrow: [[ Journal/$tomorrowsDate ]]" >> $fileName
+    Write-Output "[[ $($todaysDate.Year) ]]" >> $fileName
     Write-Output "Entry Number: $journalEntryNumber" >> $fileName
 }
 
 vim "$fileName"
+
