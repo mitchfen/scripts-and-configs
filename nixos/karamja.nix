5c326540d2fc28a8c8be7a3778dedb2dd7e6321d
Mitchell Fenner <mitch@mitchfen.xyz>
2025-05-23 07:44:09 -0400

    nix configs

---
+++ nixos/karamja.nix
@@ -0,0 +1,68 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "karamja"; # Define your hostname.
+  networking.networkmanager.enable = true;
+  time.timeZone = "America/Detroit";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_US.UTF-8";
+    LC_IDENTIFICATION = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "en_US.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_TELEPHONE = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+  };
+
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  users.users.mitchfen = {
+    isNormalUser = true;
+    description = "mitchfen";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.powershell;
+    packages = with pkgs; [];
+  };
+
+  programs.neovim = {
+    enable = true;
+    defaultEditor = true;
+  };
+
+  environment.systemPackages = with pkgs; [
+    k3s
+    kubernetes-helm
+    btop
+    powershell
+    dig
+    lf
+    screenfetch
+    gh
+    git
+  ];
+
+
+  services.openssh.enable = true;
+  networking.firewall.enable = false;
+
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
+
