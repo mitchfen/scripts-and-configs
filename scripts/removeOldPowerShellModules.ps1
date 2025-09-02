d85c268f7ea585a11859686e7e31c4abb8f7291a
Mitchell Fenner <mfenner@configit.com>
2025-09-02 12:01:12 -0400

    update powershell module removal script

--- scripts/removeOldPowerShellModules.ps1
+++ scripts/removeOldPowerShellModules.ps1
@@ -1,5 +1,6 @@
 $modules = Get-InstalledModule
 foreach ($module in $modules) {
-  Write-Host -Message "Uninstalling any old versions of $($module.Name) [Latest currently installed is $( $module.Version)]"
+  Write-Host -Message "Considering whether to remove any old versions of $($module.Name) [Latest currently installed is $( $module.Version)]"
   Get-InstalledModule -Name $module.Name -AllVersions | Where-Object {$_.Version -ne $module.Version} | Uninstall-Module -Verbose 
 }
+
